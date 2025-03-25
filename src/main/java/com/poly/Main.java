8b232be3b851c538c3b09ddcd0871ac13a8bc2fb
dorisanse <huyenanhbui021021@gmail.com>
2025-03-26 00:06:29 +0700

    Add printItems function to demonstrate nested loops and O(n^2) complexity

--- src/main/java/com/poly/Main.java
+++ src/main/java/com/poly/Main.java
@@ -4,7 +4,10 @@ package com.poly;
 public class Main {
     public static void printItems(int n) {
         for (int i = 0; i < n; i++) {
-            System.out.println(i);
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+
         }
 
     }
