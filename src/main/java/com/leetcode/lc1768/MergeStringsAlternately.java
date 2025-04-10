2e058d88a55c14884e092ff6c64d1b8b3048770a
dorisanse <huyenanhbui021021@gmail.com>
2025-04-10 22:09:53 +0700

    solve Leetcode 1768 - Merge Strings Alternately

---
+++ src/main/java/com/leetcode/lc1768/MergeStringsAlternately.java
@@ -0,0 +1,23 @@
+package com.leetcode.lc1768;
+
+public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int lent1 = word1.length();
+        int lent2 = word2.length();
+        int maxLen = Math.max(lent1, lent2);
+
+
+        for(int i = 0; i < maxLen ; i++) {
+            if (i < lent1) sb.append(word1.charAt(i));
+            if (i < lent2) sb.append(word2.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MergeStringsAlternately m = new MergeStringsAlternately();
+         String result = m.mergeAlternately("abcd", "uioklhg");
+        System.out.printf(result);
+    }
+}
